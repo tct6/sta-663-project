e577bb16bb65ae3e1ce92b0aa0286adc8b9fe0ee
tct6 <tracy.truong@duke.edu>
2015-04-30 16:20:41 -0400

    final edits completed

--- src/QT_analysis_functions.py
+++ src/QT_analysis_functions.py
@@ -66,7 +66,7 @@ def EM_Y1(Y, G, Z, b_0, g_0, sigma_11_0, q_0, tol=1e-6, max_iter=1000):
         ll_new = 0.0
         
         # E-step
-        psi_n2 = []
+        psi_n2 = [] # psi matrix
         mus_m = np.dot(G_uni,b1) + np.dot(Z_uni,g1)
         for i in range(n1,n):
             numer = stats.norm.pdf(Y[i,0],mus_m,np.sqrt(sigma_11))
@@ -96,7 +96,7 @@ def EM_Y1(Y, G, Z, b_0, g_0, sigma_11_0, q_0, tol=1e-6, max_iter=1000):
             break
 
         ll_old = ll_new
-    Omega_1 = Y1_covar(Y, G, Z, b1, g1, sigma_11, q, psi_n, m)
+    Omega_1 = Y1_covar(Y, G, Z, b1, g1, sigma_11, q, psi_n, m) # estimate covariance matrix
     return(b1,g1,sigma_11,q,Omega_1)
 
 
@@ -110,7 +110,7 @@ def unique_pairs(G, Z):
     n1 = G.shape[0]
     chain_GZ = [zip(l,list(chain(*Z.tolist()))) for l in combinations(list(chain(*G.tolist())),len(Z))]
     GZ = list(chain(*chain_GZ))
-    unique_gz = list(Set(GZ))
+    unique_gz = list(Set(GZ)) # obtain unique pairs through Set
     dict_gz = dict(zip(iter(unique_gz),range(len(unique_gz))))
     gz_value_frm_dict = [dict_gz[l] for l in GZ]
 
@@ -123,7 +123,7 @@ def unique_pairs(G, Z):
         for r in range(ind_gz.shape[0]):
             ind_gz[r,gz_value_frm_dict[r]] = 1 
     
-    gz_arr = np.array(unique_gz)
+    gz_arr = np.array(unique_gz) # indicator matrix for which sample corresponds to which unique pair
     g_new = gz_arr[:,0].reshape((gz_arr.shape[0],1))
     z_new = gz_arr[:,1].reshape((gz_arr.shape[0],1))
 
@@ -149,14 +149,15 @@ def Y1_covar(Y, G, Z, b1, g1, sigma_11, q, psi_n, m):
     grad_sym = sympy.Matrix([b, g, s, q_s])
     l1 = [sympy.diff(U, sym) for sym in grad_sym]
     l2 = sympy.Matrix(sympy.hessian(U, grad_sym))
-    f_l1 = sympy.lambdify((b, g, s, q_s, y_s, G_s, Z_s), l1[0:3], 'numpy')
-    f_l2 = sympy.lambdify((b, g, s, q_s, y_s, G_s, Z_s), l2[0:3,0:3], 'numpy')
+    f_l1 = sympy.lambdify((b, g, s, q_s, y_s, G_s, Z_s), l1[0:3], 'numpy') # allows for easy substitution into derivatives
+    f_l2 = sympy.lambdify((b, g, s, q_s, y_s, G_s, Z_s), l2[0:3,0:3], 'numpy') # allows for easy substitution into derivatives
 
-    fl1_qs = sympy.lambdify(q_s, l1[3], 'numpy')
+    fl1_qs = sympy.lambdify(q_s, l1[3], 'numpy')  
     l1_qs = np.array(fl1_qs(q)).reshape((m,1))
     fl2_qs = sympy.lambdify(q_s, l2[3,3], 'numpy')
     l2_qs = np.append(np.zeros((3,m)),np.diag(fl2_qs(q)),axis=0)
-
+    
+    # There are 4 different sums needed for the estimation of covariance matrix
     part1Q = 0.0
     part2Q = 0.0
     part4Q = 0.0
@@ -173,7 +174,7 @@ def Y1_covar(Y, G, Z, b1, g1, sigma_11, q, psi_n, m):
         part4Q += np.dot(part3Q,part3Q.T)
     Q_1 = -part1Q - part2Q + part4Q
     
-    D = np.append((np.identity(m+2)),[[0,0,0]+[-1]*(m-1)],axis=0)
+    D = np.append((np.identity(m+2)),[[0,0,0]+[-1]*(m-1)],axis=0) # reflect the contraint of sum(q_j) = 1
     F = np.dot(np.dot(D.T,Q_1),D)
     Omega_1 = la.inv(F)
     return(Omega_1)
@@ -193,10 +194,10 @@ def Y2_est(Y1, G1, Z1, b1, g1, Y2, G2, Z2, Omega_bg_1):
     n2 = Y2.shape[0]
     Y1_hat = Y1 - np.dot(G1,b1) - np.dot(Z1,g1)
     X = np.column_stack((Y1_hat,G2,Z2))
-    est = sm.OLS(Y2,X).fit()
+    est = sm.OLS(Y2,X).fit() # OLS estimates of parameters
     delta, b2, g2 = est.params
-    sigma_22 = (1./n2)*((Y2 - delta*Y1_hat - np.dot(G2,b2) - np.dot(Z2,g2))**2).sum()
-    Omega_2 = Y2_covar(delta,b2,g2,sigma_22,b1,g1,Y1,Y2,G1,G2,Z1,Z2,Omega_bg_1)
+    sigma_22 = (1./n2)*((Y2 - delta*Y1_hat - np.dot(G2,b2) - np.dot(Z2,g2))**2).sum() # obtain sigma_22
+    Omega_2 = Y2_covar(delta,b2,g2,sigma_22,b1,g1,Y1,Y2,G1,G2,Z1,Z2,Omega_bg_1) # covariance matrix estimation
     
     return(delta, b2, g2, sigma_22, Omega_2)
 
@@ -223,7 +224,7 @@ def Y2_covar(delta, b2, g2, sigma_22, b1, g1, Y_1, Y_2, G_1, G_2, Z_1, Z_2, Omeg
     U = np.array([np.dot(Y1_hat.T,Y1_hat),Y1_G2,Y1_Z2,Y1_G2,G2_2,G2_Z2,Y1_Z2,G2_Z2,Z2_2]).reshape((3,3))
     V = np.array([np.dot(Y_2.T,Y1_hat),np.dot(Y_2.T,G_2),np.dot(Y_2.T,Z_2)]).reshape((3,1))
     dU_b1 = np.array([-2*G_1.sum()+2*b1*G2_2+2*g1*G1_Z1,-np.dot(G_1.T,G_2),-np.dot(G_1.T,Z_2),-np.dot(G_1.T,G_2),[0],[0],
-                      -np.dot(G_1.T,Z_2),[0],[0]]).reshape((3,3))
+                      -np.dot(G_1.T,Z_2),[0],[0]]).reshape((3,3)) 
     dV_b1 = np.array([-np.dot(G_1.T,Y_2),[0],[0]]).reshape((3,1))
     dU_g1 = np.array([-2*Z_1.sum()+2*g1*Z2_2+2*b1*G1_Z1,-np.dot(G_2.T,Z_1),-np.dot(Z_1.T,Z_2),
                       -np.dot(G_2.T,Z_1),[0],[0],-np.dot(Z_1.T,Z_2),[0],[0]]).reshape((3,3))
